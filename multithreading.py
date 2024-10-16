aa1bd06bdfe2b9e57839fc0a5136d86d2a06ea5f
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2024-10-17 00:51:29 +0530

    Create multithreading.py

---
+++ multithreading.py
@@ -0,0 +1,16 @@
+import threading
+def print_cube(n):
+    print("Cube:",n * n * n)
+
+def print_square(n):
+    print("Square:",n*n*n)
+
+
+t1 = threading.Thread(print_square(10))
+t2 = threading.Thread(print_cube(10))
+
+t1.start()
+t2.start()
+
+t1.join()
+t2.join()
