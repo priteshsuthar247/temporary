c475f394f05cd22832f1d6dd18e1e7efb3f23af2
Pritesh Suthar <124127811+priteshsuthar247@users.noreply.github.com>
2024-10-17 07:22:11 +0530

    Update operator.py

--- operator.py
+++ operator.py
@@ -1,10 +1,8 @@
 class demo:
     x=0
     def __add__(a,b):
-        return b.x + a.x
+        return a.x + b.x
 
-d1 = demo()
-d2 = demo()
-d1.x = 10
-d2.x = 20
-print(d1+d2)
+a = demo()
+a.x = 10
+print(a+a)
